7d060e398e3b280b7cd5fe252942d5adf2176d00
lixuan <455636959@qq.com>
2022-11-14 11:59:38 +0800

    '修改配置'

--- celery_tasks/config.py
+++ celery_tasks/config.py
@@ -4,6 +4,6 @@
 # backend = 'redis://172.16.10.7:6381/1'
 
 
-BROKER_URL = "redis://127.0.0.1:6379/0"
+broker_url = "redis://127.0.0.1:6379/0"
 
-CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/1"
+result_backend  = "redis://127.0.0.1:6379/1"
